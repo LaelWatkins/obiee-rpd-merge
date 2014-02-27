1e3e14a703f7689ce844a84de281001ea433ad6e
Lael Watkins <laelwatkins@gmail.com>
2014-02-27 11:47:21 -0500

    changed method names

--- env/src/main/java/org/ccci/obiee/rpd/env/ObieeConnPoolPasswdSwitch.java
+++ env/src/main/java/org/ccci/obiee/rpd/env/ObieeConnPoolPasswdSwitch.java
@@ -24,104 +24,102 @@ import java.util.Properties;
 
 public class ObieeConnPoolPasswdSwitch 
 {
-	public static void main(String[] args) throws IOException 
-	{	    
-	    Properties props = new Properties();
+    public static void main(String[] args) throws IOException 
+    {	    
+	Properties props = new Properties();
 
-		 try 
-		 {
-		     //Not used yet
-		     URL url = ObieeConnPoolPasswdSwitch.class.getClassLoader().getResource("ObieeRpdXmlFiles.properties");
-		     props.load(url.openStream());
-		     //Not used yet
-		     
-		     mergeRpdCredentialsWithChangesXML(retrieveConnPoolCreds(props));
-		 } 
-		 catch (IOException e)
-		 {			
-			 System.err.println( "Caught an IOException: "  + e.getMessage());
-			    throw new IOException(e);			 
-		 }				
-	}
-
-	private static Map<String, String> retrieveConnPoolCreds(Properties props) 
+	try 
 	{
-	    Map<String,String> mapConnectionPoolUsers = new HashMap<String,String>();
-	    
-	    try {
+	    //*********Not used yet
+	    URL url = ObieeConnPoolPasswdSwitch.class.getClassLoader().getResource("ObieeRpdXmlFiles.properties");
+	    props.load(url.openStream());
+	    //******Not used yet
 
-	    	Document doc = getRpdXMLDocument(new File("C:\\files\\OracleBIAnalyticsAppsStage.xml"));	    	
-	    	
-	    	doc.getDocumentElement().normalize();			 
-	    	NodeList nList = doc.getElementsByTagName("ConnectionPool");
+	    mergeRpdCredentialsSourceRpd(retrieveConnPoolCredsTargetEnv(props));
+	} 
+	catch (IOException e)
+	{			
+	    System.err.println( "Caught an IOException: "  + e.getMessage());
+	    throw new IOException(e);			 
+	}				
+    }
 
-	    	for (int temp = 0; temp < nList.getLength(); temp++) 
-	    	{
-	    		Node nNode = nList.item(temp);
+    private static Map<String, String> retrieveConnPoolCredsTargetEnv(Properties props) 
+    {
+	Map<String,String> mapConnectionPoolUsers = new HashMap<String,String>();
 
-	    		if (nNode.getNodeType() == Node.ELEMENT_NODE) 
-	    		{
-	    			Element eElement = (Element) nNode;
-	    			mapConnectionPoolUsers.put(eElement.getAttribute("parentName") ,eElement.getAttribute("password") );
-	    		}
-	    	}
-	    	
-	    	
-	    } 
-	    catch (Exception e) 
+	try 
+	{
+	    Document doc = getRpdXMLDocument(new File("C:\\files\\OracleBIAnalyticsAppsStage.xml"));	    	
+
+	    doc.getDocumentElement().normalize();			 
+	    NodeList nList = doc.getElementsByTagName("ConnectionPool");
+
+	    for (int temp = 0; temp < nList.getLength(); temp++) 
 	    {
-	    	e.printStackTrace();
-	    }
-	    
-	    return mapConnectionPoolUsers;
-	}
-  
-	private static void mergeRpdCredentialsWithChangesXML(Map<String,String> map)
-	{	
-		try {
-			
-			StreamResult result = new StreamResult(new File("C:\\files\\OracleBIAnalyticsApps.xml"));
-			Document xmlDoc = getRpdXMLDocument(new File("C:\\files\\OracleBIAnalyticsAppsTest.xml"));				
-			Transformer transformer = TransformerFactory.newInstance().newTransformer();
-						
-			xmlDoc.getDocumentElement().normalize();			 
-			NodeList nList = xmlDoc.getElementsByTagName("ConnectionPool");
-						
-			for (int temp = 0; temp < nList.getLength(); temp++) 
-			{
-				Node nNode = nList.item(temp);
-				Element eElement = (Element) nNode;
-				
-				if (nNode.getNodeType() == Node.ELEMENT_NODE) 
-				{
-					for(Map.Entry<String, String>entry: map.entrySet())		
-					{			
-						if( eElement.getAttribute("parentName").equals(entry.getKey()) )
-						{		
-							eElement.setAttribute(eElement.getAttribute("password"), entry.getValue());							
-						}					
-					}
-				} 
-			}
-			
-			transformer.transform(new DOMSource(xmlDoc), result);
-			//System.out.println("Done");
-		} 
-		catch (Exception e) 
+		Node nNode = nList.item(temp);
+
+		if (nNode.getNodeType() == Node.ELEMENT_NODE) 
 		{
-			e.printStackTrace();
+		    Element eElement = (Element) nNode;
+		    mapConnectionPoolUsers.put(eElement.getAttribute("parentName") ,eElement.getAttribute("password") );
 		}
+	    }
+	} 
+	catch (Exception e) 
+	{
+	    e.printStackTrace();
 	}
-	
-	
-	private static Document getRpdXMLDocument(File fXmlFile) throws ParserConfigurationException, SAXException, IOException 
-	{		
-		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-		Document doc = dBuilder.parse(fXmlFile);
-		return doc;
+
+	return mapConnectionPoolUsers;
+    }
+    
+    private static void mergeRpdCredentialsSourceRpd(Map<String,String> map)
+    {	
+	try {
+
+	    StreamResult result = new StreamResult(new File("C:\\files\\OracleBIAnalyticsApps.xml"));
+	    Document xmlDoc = getRpdXMLDocument(new File("C:\\files\\OracleBIAnalyticsAppsTest.xml"));				
+	    Transformer transformer = TransformerFactory.newInstance().newTransformer();
+
+	    xmlDoc.getDocumentElement().normalize();			 
+	    NodeList nList = xmlDoc.getElementsByTagName("ConnectionPool");
+
+	    for (int temp = 0; temp < nList.getLength(); temp++) 
+	    {
+		Node nNode = nList.item(temp);
+		Element eElement = (Element) nNode;
+
+		if (nNode.getNodeType() == Node.ELEMENT_NODE) 
+		{
+		    for(Map.Entry<String, String>entry: map.entrySet())		
+		    {			
+			if( eElement.getAttribute("parentName").equals(entry.getKey()) )
+			{		
+			    eElement.setAttribute(eElement.getAttribute("password"), entry.getValue());							
+			}					
+		    }
+		} 
+	    }
+
+	    transformer.transform(new DOMSource(xmlDoc), result);
+	    //System.out.println("Done");
+	} 
+	catch (Exception e) 
+	{
+	    e.printStackTrace();
 	}
-	
+    }
+
+
+    private static Document getRpdXMLDocument(File fXmlFile) throws ParserConfigurationException, SAXException, IOException 
+    {		
+	DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+	DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+	Document doc = dBuilder.parse(fXmlFile);
+	return doc;
+    }
+
 }
 
 
